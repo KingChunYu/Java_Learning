3d00d6878c69fa9659d3b334102b3a74ffccf13b
chunyu <jcy23401@163.com>
2016-08-22 20:21:53 +0800

    提交camera练习。

--- Android学习Demo/CammerTest/app/src/main/java/com/example/chunyu/cammertest/MainActivity.java
+++ Android学习Demo/CammerTest/app/src/main/java/com/example/chunyu/cammertest/MainActivity.java
@@ -1,13 +1,89 @@
 package com.example.chunyu.cammertest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button takePhoto;
+    ImageView picture;
+    Uri imageUri;
+    private final int TAKEPHOTO = 0;
+    private final int CLIPPHOTO = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        takePhoto = (Button) findViewById(R.id.btn_take_photo);
+        picture = (ImageView) findViewById(R.id.picture);
+
+        takePhoto.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_take_photo:
+                takePhoto();
+                break;
+        }
+    }
+
+    private void takePhoto() {
+        File imageFile = new File(Environment.getExternalStorageDirectory(), "outImage.jpg");
+        if (imageFile.exists()) {
+            imageFile.delete();
+        }
+
+        try {
+            imageFile.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        imageUri = Uri.fromFile(imageFile);
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKEPHOTO);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case TAKEPHOTO:
+                if (resultCode == RESULT_OK) {
+                    Intent intent = new Intent("com.android.camera.action.CROP");
+                    intent.setDataAndType(imageUri, "image/*");
+                    intent.putExtra("scale", true);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, CLIPPHOTO);
+                }
+                break;
+            case CLIPPHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
     }
 }
